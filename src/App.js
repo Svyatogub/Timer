f590f0f745f82bdabf0c68b291aeb936526af6c9
Svyatogub <svyatogubzemlyanichkeen@gmail.com>
2021-12-22 19:57:23 +0200

    end

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css"
 import Timer from "./components/Timer";
 
 import { interval } from "rxjs";
-import { map, pipe } from "rxjs/operators";
+import { map } from "rxjs/operators";
 
 
 function App() {
@@ -11,7 +11,7 @@ function App() {
   const [timer, setTimer] = useState(0);
   const [diff, setDiff] = useState(0);
 
-  const [subscription, setSubscription] = useState("");
+  const [subscription, setSubscription] = useState(null);
   const [prevent, setPrevent] = useState(true);
 
   const onStartHandler = () => {
@@ -43,7 +43,7 @@ function App() {
       }
 
       setDiff(timer);
-      setSubscription("");
+      setSubscription(null);
     }
   };
 
@@ -71,76 +71,5 @@ function App() {
       </div>
     </div>
   );
-
-
-
-
-
-
-
-
-  /*const [timer, setTimer] = useState(0);
-  const [diff, setDiff] = useState(0);
-
-  const [subscription, setSubscription] = useState("");
-  const [prevent, setPrevent] = useState(true);
-
-  const onStartHandler = () => {
-    if (!subscription) {
-      const timerSubscription = interval(1000)
-        .pipe(map((v) => v + 1))
-        .subscribe((v) => {
-          setTimer(v + diff);
-        });
-      setSubscription(timerSubscription);
-    } else {
-      subscription.unsubscribe();
-      setTimer(0);
-      setDiff(0);
-      setSubscription("");
-    }
-  };
-
-  const onWaitHandler = (event) => {
-    if (prevent) {
-      setPrevent(false);
-      const timerInstance = setTimeout(function () {
-        setPrevent(true);
-        clearTimeout(timerInstance);
-      }, 300);
-    } else {
-      if (subscription) {
-        subscription.unsubscribe();
-      }
-
-      setDiff(timer);
-      setSubscription("");
-    }
-  };
-
-  const onResetHandler = () => {
-    if (subscription) {
-      subscription.unsubscribe();
-    }
-
-    const timerSubscription = interval(1000).subscribe((v) => {
-      setTimer(v);
-    });
-    setSubscription(timerSubscription);
-  };
-
-  return (
-    <div onClick={onWaitHandler} className="Block">
-      <div className="Content">
-        <div className="TimeLeft">
-          <Timer Countdown={timer ? timer : diff} />
-        </div>
-        <div className="Buttons">
-          <button onClick={onStartHandler}>start/stop</button>
-          <button onClick={onResetHandler}>reset</button>
-        </div>
-      </div>
-    </div>
-  );*/
 }
 export default App;
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,5 +1,4 @@
 import React, { useEffect, useState } from "react";
-import { Observable } from "rxjs";
 
 const Timer = (props) => {
   const [hours, setHours] = useState(0);
